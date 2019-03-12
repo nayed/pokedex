8e949fd9d0028140caeb864e575f82130953609c
Nayed Saïd Ali <nayed.saidali@gmail.com>
2019-03-12 13:30:01 +0100

    Updates

---
+++ precache-manifest.fd686dab4b77b3a4bc07aeb92a8b0b76.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fb984f6761985509fcccfdaf8ad28698",
+    "url": "/pokedex/static/media/pokeball.fb984f67.svg"
+  },
+  {
+    "revision": "499fa032a833d26aac94",
+    "url": "/pokedex/static/js/runtime~main.fa119ff4.js"
+  },
+  {
+    "revision": "17f2e2ffc761ac8cc2b2",
+    "url": "/pokedex/static/js/main.769c8867.chunk.js"
+  },
+  {
+    "revision": "9edb78e18caac8852748",
+    "url": "/pokedex/static/js/2.407bce1b.chunk.js"
+  },
+  {
+    "revision": "42f1bb4c68e6fc75c6e231e3d3a9d896",
+    "url": "/pokedex/index.html"
+  }
+];
